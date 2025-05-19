8eb21cb911de41356553046042c9dfb0d9c36f19
Oscar200301 <21030082@itesa.edu.mx>
2025-05-19 10:55:43 -0600

    llamado a archivos

--- menu.py
+++ menu.py
@@ -5,8 +5,9 @@ print('3- Multiplicacion')
 
 numero = int(input('Elige una opcion: '))
 if numero == 1:
-    
+    exec(open('Suma.py').read())
 elif numero == 2:
-    
+    exec(open('Resta.py').read())
 elif numero == 3:
+    exec(open('Multiplicacion.py').read())
     
