f85921d1367ee12ced669ea4e84ff09ce3db4a48
Oscar200301 <21030082@itesa.edu.mx>
2025-05-19 10:39:11 -0600

    Menu v1

---
+++ menu.py
@@ -0,0 +1,12 @@
+print('Calculadora')
+print('1- Suma')
+print('2- Resta')
+print('3- Multiplicacion')
+
+numero = int(input('Elige una opcion: '))
+if numero == 1:
+    
+elif numero == 2:
+    
+elif numero == 3:
+    
